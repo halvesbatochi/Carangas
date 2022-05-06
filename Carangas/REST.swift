b1a0ddbc8a91e3edf7c28283d8b45f1a94e15f37
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-05-06 13:13:02 -0300

    Closures, metodo GET - API

--- Carangas/REST.swift
+++ Carangas/REST.swift
@@ -8,9 +8,18 @@
 
 import Foundation
 
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
 class REST {
     
-    private static let basePath = "htpps://carangas.herokuapp.com/cars"
+    private static let basePath = "https://carangas.herokuapp.com/cars"
     private static let configuration: URLSessionConfiguration = {
         let config = URLSessionConfiguration.default
         config.allowsCellularAccess = false
@@ -19,7 +28,36 @@ class REST {
         config.httpMaximumConnectionsPerHost = 5
         return config
     }()
-    private static let sesssion = URLSession(configuration: configuration)
+    private static let session = URLSession(configuration: configuration)
     
-    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else { return }
+                    do {
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        onComplete(cars)
+                    } catch {
+                        print(error.localizedDescription)
+                        onError(.invalidJSON)
+                    }
+                } else {
+                    print("Algum status inválido pelo servidor!")
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+            } else {
+                onError(.taskError(error: error!))
+            }
+        }
+        dataTask.resume()
+    }
 }
