4e67b720b4652ffef3789ec2e57f114e2d2ce3ce
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-05-09 16:14:58 -0300

    Complete CRUD

--- Carangas/AddEditViewController.swift
+++ Carangas/AddEditViewController.swift
@@ -17,14 +17,49 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var scGasType: UISegmentedControl!
     @IBOutlet weak var btAddEdit: UIButton!
     @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    // MARK: Properties
+    var car: Car!
 
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        if car != nil {
+            tfBrand.text = car.brand
+            tfName.text = car.name
+            tfPrice.text = "R$ \(car.price)"
+            scGasType.selectedSegmentIndex = car.gasType
+            btAddEdit.setTitle("Alterar carro", for: .normal)
+        }
     }
     
     // MARK: - IBActions
     @IBAction func addEdit(_ sender: UIButton) {
+        if car == nil {
+            car = Car()
+        }
+        car.name = tfName.text!
+        car.brand = tfBrand.text!
+        if tfPrice.text!.isEmpty { tfPrice.text = "0" }
+        car.price = Double(tfPrice.text!)!
+        car.gasType = scGasType.selectedSegmentIndex
+        
+        if car._id == nil {
+            REST.save(car: car) { (success) in
+                self.goBack()
+            }
+        } else {
+            REST.update(car: car) { (success) in
+                self.goBack()
+            }
+        }
+    }
+    
+    // MARK: Methods
+    func goBack() {
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
 
 }
